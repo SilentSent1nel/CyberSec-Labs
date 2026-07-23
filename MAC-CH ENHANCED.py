82411daaeb049d7f0e3e2a71019dd9e15093ff1e
Mirkan-23 <mirkan070@outlook.com>
2026-07-24 00:59:27 +0200

    Argumenten ophalen verplaatst naar aparte functie

--- MAC-CH ENHANCED.py
+++ MAC-CH ENHANCED.py
@@ -3,6 +3,18 @@
 import subprocess
 import optparse
 
+def argumenten_ophalen():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i", "--interface", dest="interface", help="Interface waarvan de MAC-Adres wordt gewijzigd")
+    parser.add_option("-m", "--mac-adres", dest="Nieuwe_MAC", help="Dit wordt de nieuwe MAC-Adres dat gebuikt gaat worden")
+
+    return parser.parse_args()
+
+    print("Actieve interfaces:")
+    subprocess.call("ip -o link show up | awk -F': ' '{print $2}'", shell=True)
+
+
 def mac_wijzigen(interface, Nieuwe_MAC):
     print("\n")
 
@@ -13,14 +25,7 @@ def mac_wijzigen(interface, Nieuwe_MAC):
     print(f"[+] MAC adres veranderen voor: interface {interface} naar {Nieuwe_MAC} - :")
     subprocess.call(["ifconfig", interface])
 
-parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface waarvan de MAC-Adres wordt gewijzigd")
-parser.add_option("-m", "--mac-adres", dest="Nieuwe_MAC", help="Dit wordt de nieuwe MAC-Adres dat gebuikt gaat worden")
 
-(options, arguments) = parser.parse_args()
-
-print("Actieve interfaces:")
-subprocess.call("ip -o link show up | awk -F': ' '{print $2}'", shell=True)
-
+(options, arguments) = argumenten_ophalen()
 mac_wijzigen(options.interface, options.Nieuwe_MAC)
