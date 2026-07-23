9992afdbea28db42617f7213e35681e9e89ef0d2
Mirkan-23 <mirkan070@outlook.com>
2026-07-24 01:31:40 +0200

    Argumentvalidatie en gekleurde uitvoer toegevoegd

--- MAC-CH ENHANCED.py
+++ MAC-CH ENHANCED.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+from colorama import Fore, Back, Style
 
 def argumenten_ophalen():
     parser = optparse.OptionParser()
@@ -9,7 +10,13 @@ def argumenten_ophalen():
     parser.add_option("-i", "--interface", dest="interface", help="Interface waarvan de MAC-Adres wordt gewijzigd")
     parser.add_option("-m", "--mac-adres", dest="Nieuwe_MAC", help="Dit wordt de nieuwe MAC-Adres dat gebuikt gaat worden")
 
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("[-] Voer een interface in, gebruik -h of --help voor meer info")
+    elif not options.Nieuwe_MAC:
+        parser.error("[-] Voer een nieuwe MAC adres in, gebruik -h of --help voor meer info")
+    return options
 
     print("Actieve interfaces:")
     subprocess.call("ip -o link show up | awk -F': ' '{print $2}'", shell=True)
@@ -22,10 +29,11 @@ def mac_wijzigen(interface, Nieuwe_MAC):
     subprocess.call(["ifconfig", interface, "hw", "ether", Nieuwe_MAC])
     subprocess.call(["ifconfig", interface, "up"])
 
-    print(f"[+] MAC adres veranderen voor: interface {interface} naar {Nieuwe_MAC} - :")
+    print(f"{Fore.LIGHTWHITE_EX} [+] MAC adres veranderen voor: interface {Fore.LIGHTRED_EX}{interface}{Style.RESET_ALL} {Fore.LIGHTWHITE_EX} naar: {Fore.LIGHTYELLOW_EX}{Nieuwe_MAC}{Style.RESET_ALL}")
+    print(Fore.LIGHTGREEN_EX)
     subprocess.call(["ifconfig", interface])
 
 
 
-(options, arguments) = argumenten_ophalen()
+options = argumenten_ophalen()
 mac_wijzigen(options.interface, options.Nieuwe_MAC)
