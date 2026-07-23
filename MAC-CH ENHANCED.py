595fd5f0b250f1aefc99b3f72fab49ee0c5e8e6d
Mirkan-23 <mirkan070@outlook.com>
2026-07-24 00:45:29 +0200

    MAC-wijziging verwerkt ihn aparte functie

--- MAC-CH ENHANCED.py
+++ MAC-CH ENHANCED.py
@@ -3,6 +3,16 @@
 import subprocess
 import optparse
 
+def mac_wijzigen(interface, Nieuwe_MAC):
+    print("\n")
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", Nieuwe_MAC])
+    subprocess.call(["ifconfig", interface, "up"])
+
+    print(f"[+] MAC adres veranderen voor: interface {interface} naar {Nieuwe_MAC} - :")
+    subprocess.call(["ifconfig", interface])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface waarvan de MAC-Adres wordt gewijzigd")
@@ -13,16 +23,4 @@ parser.add_option("-m", "--mac-adres", dest="Nieuwe_MAC", help="Dit wordt de nie
 print("Actieve interfaces:")
 subprocess.call("ip -o link show up | awk -F': ' '{print $2}'", shell=True)
 
-print("\n")
-
-interface = options.interface
-Nieuwe_MAC = options.Nieuwe_MAC
-
-print("\n")
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", Nieuwe_MAC])
-subprocess.call(["ifconfig", interface, "up"])
-
-print(f"[+] MAC adres veranderen voor: interface {interface} naar {Nieuwe_MAC} - :")
-subprocess.call(["ifconfig", interface])
+mac_wijzigen(options.interface, options.Nieuwe_MAC)
